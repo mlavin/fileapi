789ed09a57dd8e63d78f2105f97eeedddb2e4e0c
mlavin <markdlavin@gmail.com>
2014-11-01 13:37:20 -0400

    Adding auth and content types to the INSTALLED_APPS. Adding API token view.

--- fileapi/settings.py
+++ fileapi/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.staticfiles',
     'fileapi',
 )
--- fileapi/urls.py
+++ fileapi/urls.py
@@ -1,3 +1,5 @@
+import jwt_auth.views
+
 from django.conf.urls import url
 
 from .views import FileListView, FileDetailView, IndexView
@@ -6,5 +8,6 @@ from .views import FileListView, FileDetailView, IndexView
 urlpatterns = [
     url(r'^uploads/$', FileListView.as_view(), name='upload-list'),
     url(r'^uploads/(?P<name>.*)', FileDetailView.as_view(), name='upload-detail'),
+    url(r'^api-token/', jwt_auth.views.obtain_jwt_token, name='api-token'),
     url(r'^$', IndexView.as_view(), name='homepage')
 ]
