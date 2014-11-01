8d5d1e153c69a65fc42e6f4d1c2228be3c312248
mlavin <markdlavin@gmail.com>
2014-11-01 13:18:14 -0400

    New view to handle the drag and drop to upload new files to the API.

--- fileapi/static/js/main.js
+++ fileapi/static/js/main.js
@@ -54,9 +54,73 @@
         }
     });
 
-    var listView = new UploadListingView();
+    var NewUploadView = Backbone.View.extend({
+        el: '#upload',
+        events: {
+            'dragenter': 'enter',
+            'dragover': 'over',
+            'dragend': 'end',
+            'dragleave': 'end',
+            'drop': 'drop'
+        },
+        enter: function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.$el.addClass('hover');
+            return false;
+        },
+        over: function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.$el.addClass('hover');
+            return false;
+        },
+        end: function (e) {
+            this.$el.removeClass('hover');
+        },
+        drop: function (e) {
+            var formData = new FormData(),
+                file;
+            e.stopPropagation();
+            e.preventDefault();
+            if (e.originalEvent.dataTransfer.files.length === 0) {
+                return;
+            }
+            file = e.originalEvent.dataTransfer.files[0];
+            formData.append('name', file);
+            uploads.create({}, {
+                error: $.proxy(this.fail, this),
+                wait: true,
+                data: formData,
+                processData: false,
+                contentType: false
+            });
+            this.end();
+        },
+        fail: function (_model, response) {
+            var errors = response.responseJSON.name,
+                errorTemplate = _.template('<p class="error error-<%- code %>"><%- message %></p>');
+            _.map(errors, function (error) {
+                this.$el.before(errorTemplate(error));
+            }, this);
+            setTimeout($.proxy(function() {
+                var errors = $('.error');
+                errors.fadeOut('slow', function () {
+                    errors.remove();
+                });
+            }, this), 2500);
+        },
+        render: function () {
+            this.$el.show();
+        }
+    });
+
+    var listView = new UploadListingView(),
+        newView = new NewUploadView();
+
     uploads.on('add', listView.addFile, listView);
     uploads.fetch();
     listView.render();
+    newView.render();
 
 })(jQuery, Backbone, _);
