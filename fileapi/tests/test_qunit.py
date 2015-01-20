4460aee67c1d95fd896d131add5c99151b24573e
mlavin <markdlavin@gmail.com>
2015-01-19 22:03:06 -0500

    Add Django testcase to load QUnit test suite and assert there are no failures.

---
+++ fileapi/tests/test_qunit.py
@@ -0,0 +1,35 @@
+import os
+
+from django.conf import settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test.utils import override_settings
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+@override_settings(STATICFILES_DIRS=(os.path.join(os.path.dirname(__file__), 'static'), ))
+class QunitTests(StaticLiveServerTestCase):
+    """Iteractive tests with selenium."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.browser = webdriver.PhantomJS()
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def test_qunit(self):
+        """Load the QUnit tests and check for failures."""
+
+        self.browser.get(self.live_server_url + settings.STATIC_URL + 'index.html')
+        results = WebDriverWait(self.browser, 5).until(
+            expected_conditions.visibility_of_element_located((By.ID, 'qunit-testresult')))
+        total = int(results.find_element_by_class_name('total').text)
+        failed = int(results.find_element_by_class_name('failed').text)
+        self.assertTrue(total and not failed, results.text)
