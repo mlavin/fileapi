6e028a1ebec704435a86ab4f1f5d51c8c364eb5e
mlavin <markdlavin@gmail.com>
2015-01-19 19:11:56 -0500

    Testing adding, browseing and deleting files.

--- fileapi/tests/test_functional.py
+++ fileapi/tests/test_functional.py
@@ -1,10 +1,18 @@
+import os
+import shutil
+import tempfile
+
 from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.files.storage import FileSystemStorage
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+
+from .. import views
 
 
 class FunctionalTests(StaticLiveServerTestCase):
@@ -21,9 +29,16 @@ class FunctionalTests(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        self._orig_storage = views.storage
+        views.storage = FileSystemStorage(self.temp_dir)
         self.username = 'test'
         self.password = 'test'
         User.objects.create_user(self.username, '', self.password)
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+        views.storage = self._orig_storage
 
     def test_show_login(self):
         """The login should be shown on page load."""
@@ -63,3 +78,62 @@ class FunctionalTests(StaticLiveServerTestCase):
         self.assertEqual('Invalid username/password', error.text)
         form = self.browser.find_element_by_id('login')
         self.assertTrue(form.is_displayed(), 'Login form should still be visible.')
+
+    def test_browse_files(self):
+        """Browse existing uploads."""
+
+        _, test_file = tempfile.mkstemp(dir=self.temp_dir)
+        self.login(self.username, self.password)
+        element = WebDriverWait(self.browser, 5).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, 'file')))
+        self.assertIn(os.path.basename(test_file), element.text)
+
+    def test_file_delete(self):
+        """Delete uploaded file."""
+
+        _, test_file = tempfile.mkstemp(dir=self.temp_dir)
+        self.login(self.username, self.password)
+        element = WebDriverWait(self.browser, 5).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, 'file')))
+        element.find_element_by_class_name('delete').click()
+        self.browser.implicitly_wait(1)
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_class_name('file')
+        self.assertFalse(os.path.exists(test_file))
+
+    def upload(self, filepath):
+        # Create file input for fake drag and drop
+        self.browser.execute_script('''
+            input = $('<input>', {id: 'seleniumUpload', type: 'file'}).appendTo('body');
+        ''');
+        self.browser.find_element_by_id('seleniumUpload').send_keys(filepath)
+        # Fake a file drag and drop event
+        self.browser.execute_script('''
+            event = $.Event('drop');
+            event.originalEvent = {
+                dataTransfer: {files: input.get(0).files}
+            };
+            $('#upload').trigger(event);
+        ''');
+
+    def test_file_upload(self):
+        """Drag and drop a new file upload."""
+
+        self.login(self.username, self.password)
+        _, test_file = tempfile.mkstemp()
+        with open(test_file, 'w') as f:
+            f.write('XXX')
+        self.upload(test_file)
+        element = WebDriverWait(self.browser, 5).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, 'file')))
+        self.assertIn(os.path.basename(test_file), element.text)
+
+    def test_invalid_file(self):
+        """Drag and drop a new file upload."""
+
+        self.login(self.username, self.password)
+        _, test_file = tempfile.mkstemp()
+        self.upload(test_file)
+        element = WebDriverWait(self.browser, 5).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, 'error')))
+        self.assertEqual('The submitted file is empty.', element.text)
