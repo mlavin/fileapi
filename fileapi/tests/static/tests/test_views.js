f3ec3ce9dc6f1d0f118cf0b259c930745faf5095
mlavin <markdlavin@gmail.com>
2015-01-19 21:39:08 -0500

    Demonstrate using mocks/stubs with Sinon.JS

--- fileapi/tests/static/tests/test_views.js
+++ fileapi/tests/static/tests/test_views.js
@@ -28,3 +28,48 @@ QUnit.test('Add Upload', function (assert) {
     this.view.addFile(upload);
     assert.equal($('.file', this.view.$el).length, 1);
 });
+
+QUnit.module('NewUploadView Tests', {
+    beforeEach: function () {
+        $('#qunit-fixture').append($('<div>', {id: 'upload'}));
+        this.uploads = new config.collections.UploadCollection();
+        this.view = new config.views.NewUploadView({uploads: this.uploads});
+        sinon.stub(this.uploads, 'create');
+    },
+    afterEach: function () {
+        this.view.remove();
+        this.uploads.create.restore();
+    }
+});
+
+QUnit.test('Drag Enter', function (assert) {
+    var e = $.Event('dragenter');
+    this.view.enter(e);
+    assert.equal(this.view.enter(e), false);
+    assert.ok(this.view.$el.hasClass('hover'));
+});
+
+QUnit.test('Drag Over', function (assert) {
+    var e = $.Event('dragover');
+    assert.equal(this.view.over(e), false);
+    assert.ok(this.view.$el.hasClass('hover'));
+});
+
+QUnit.test('Drag End', function (assert) {
+    var e = $.Event('dragend');
+    this.view.$el.addClass('hover');
+    this.view.end(e);
+    assert.equal(this.view.$el.hasClass('hover'), false);
+});
+
+QUnit.test('Drop File', function (assert) {
+    var e = $.Event('drop'),
+        file = {'name': 'test.txt'};
+    e.originalEvent = {
+        dataTransfer: {files: [file, ]}
+    };
+    this.view.$el.addClass('hover');
+    this.view.drop(e);
+    assert.ok(this.uploads.create.calledOnce);
+    assert.equal(this.view.$el.hasClass('hover'), false);
+});
